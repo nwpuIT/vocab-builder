9b2a8b3eea77cf302e2cd69fdb6b8acc784a310d
codertimo <codertimo@gmail.com>
2018-11-29 15:07:30 +0900

    Adding Abstract Class for vocab

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import unittest
+from vocab_builder import CharVocab, WordVocab
 
 
 class VocabUnitTest(unittest.TestCase):
-    pass
+    def test_char_vocab_build(self):
+        v = CharVocab(["안녕", "내 이름은", "뽀로로야"])
+        self.assertEqual(15, len(v.itos))
+
+    def test_word_vocab_build(self):
+        v = WordVocab(["안녕", "내 이름은", "뽀로로야"])
+        self.assertEqual(9, len(v.itos))
